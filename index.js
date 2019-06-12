1cb52ddbfcb1cce9b69ad6243e3173d6211451c4
Jamal Cumberbatch <jcsd418@yahoo.com>
2019-06-12 16:43:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(string) 
+{
+  return string.toUpperCase( )
+}
+function whisper(string) {
+return string.toLowerCase( )
+}
+function longShout (string) {
+  console.log("longShout").toUpperCase
+  }
+const spy = expect.spyOn(console, 'log').andCallThrough
